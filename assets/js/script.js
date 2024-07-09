e245f17736c8524ad4fb46859825b465979dde4d
moayed10111 <58371121+moayed10111@users.noreply.github.com>
2024-07-08 20:08:30 -0400

    working on start up js

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+const wish = document.getElementById("wish");
+const catagories = document.getElementById("catagorie");
+const img = document.getElementById("img");
+
+
+let wishBoard = [];
+
+function saveWishs(){
+    const wish = {
+        wish: wish.value.trim(),
+        catagories: catagories.value,
+        img: img.value
+    };
+    if (
+        
+    )
+}
