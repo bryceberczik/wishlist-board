fa64703b4d386ec04f319c794955f58317c538c4
moayed10111 <58371121+moayed10111@users.noreply.github.com>
2024-07-09 18:19:55 -0400

    added the logic to render the wishes

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ const wishTitle = document.getElementById("wish");
 const catagories = document.getElementById("category");
 const img = document.getElementById("image-url");
 const sumbit = document.getElementById("sumbit");
+const wishList = document.getElementById("wishes-list");
 
 let wishBoard = [];
 
@@ -23,3 +24,21 @@ sumbit.addEventListener("click", function (event) {
   event.preventDefault();
   saveWishs();
 });
+
+function renderWishes() {
+  wishList.innerHTML = "";
+
+  for (let i = 0; i < wishBoard.length; i++) {
+    const wish = wishBoard[i];
+
+    const li = document.createElement("li");
+    li.textContent = wish;
+    li.setAttribute("data-index", i);
+
+    const deletebtn = document.createElement("????");
+    deletebtn.textContent = "X";
+
+    li.appendChild(deletebtn);
+    todoList.appendChild(li);
+  }
+}
